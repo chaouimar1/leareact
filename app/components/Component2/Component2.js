3afba93a1879538440ce19c64c477e3fe1a15d1e
mer1 <chaoui.mar2@gmail.com>
2018-11-18 17:46:29 +0100

    touchables compo2

--- app/components/Component2/Component2.js
+++ app/components/Component2/Component2.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+import { AppRegistry, Text, View, StyleSheet, TouchableHighlight, TouchableOpacity } from 'react-native';
 import { red, green } from 'ansi-colors';
 
 export default class Component2 extends Component {
+    onPress() {
+        console.log('PRESSED !');
+    }
+
+    onPress2() {
+        console.log('PRESSED222 !');
+    }
+    
     render() {
         return (
             <View>
@@ -10,12 +18,23 @@ export default class Component2 extends Component {
                     <Text style={styles.mytext}>Hello meru</Text>
                 </View>
                 <View style={styles.container}>
-                    <View style={styles.v1}>
-                        <Text>View 1</Text>
-                    </View>
-                    <View style={styles.v2}>
-                        <Text>View 2</Text>
-                    </View>
+                    <TouchableHighlight 
+                        style={styles.v1} 
+                        onPress={this.onPress}
+                        underlayColor="blue"
+                        >
+                        <View>
+                            <Text>TouchableHighlight</Text>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableOpacity 
+                        style={styles.v2}
+                        onPress={this.onPress2}
+                        >
+                        <View>
+                            <Text>TouchableOpacity</Text>
+                        </View>
+                    </TouchableOpacity>
                     <View style={styles.v3}>
                         <Text style={styles.vtext}>View 3</Text>
                     </View>
