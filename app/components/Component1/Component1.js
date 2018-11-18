995227bcfc2f0437ed7bb0decb6449c54b1ec8e5
mer1 <chaoui.mar2@gmail.com>
2018-11-18 17:03:29 +0100

    ok

--- app/components/Component1/Component1.js
+++ app/components/Component1/Component1.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 
 export default class component1 extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            name: 'meru',
+            showName: true,
+            message: this.props.message
+        }
+    }
+
+    static defaultProps = {
+        message: 'Yo rozuya!'
+    }
+
     render() {
+        console.log('RELOADED !');
+        let name = this.state.showName ? this.state.name : 'No name';
         return (
             <View>
-                <Text>this is Component1.</Text>
+                <Text>{this.props.message}</Text>
+                <Text>{name}</Text>
             </View>
         );
     }
