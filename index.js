eaa6e8f850f78901ccfdabcf5ee812ea404b376d
mer1 <chaoui.mar2@gmail.com>
2018-11-18 16:25:23 +0100

    component1 added!

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-/** @format */
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import Component1 from './app/components/Component1/Component1'
+export default class leareact extends Component {
+    render() {
+        return (
+            <View>
+                <Component1/>
+            </View>
+        );
+    }
+}
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('leareact', () => leareact);
